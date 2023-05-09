c7bd0dce81b42040e5bc44c93839af8f8b898f02
Rahul7044 <rahulkumar581999@gmail.com>
2023-05-09 22:17:09 +0530

    Login-Screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Registration from "./components/Auth/Registration";
+import Login from "./components/Auth/Login";
+import Home from "./components/pages/Home";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 function App() {
+  const isLogin = useSelector((state) => state.auth.isLoggedIn);
+  console.log(isLogin);
   return (
     <>
-      <Registration />
+      <Switch>
+        {!isLogin && (
+          <Route path="/" exact>
+            <Redirect to="login" />
+          </Route>
+        )}
+
+        {!isLogin && (
+          <Route path="/login">
+            <Login />
+          </Route>
+        )}
+
+        {!isLogin && (
+          <Route path="/register">
+            <Registration />
+          </Route>
+        )}
+        {isLogin && (
+          <Route path="/home">
+            <Home />
+          </Route>
+        )}
+      </Switch>
     </>
   );
 }
-
 export default App;
