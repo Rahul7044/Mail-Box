2cab2a1290de4e8c8ada0f70cf59677dd34c1d0f
Rahul7044 <rahulkumar581999@gmail.com>
2023-05-09 13:31:33 +0530

    SignUp screen

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Registration from "./components/Auth/Registration";
+
+function App() {
+  return (
+    <>
+      <Registration />
+    </>
+  );
+}
+
+export default App;
